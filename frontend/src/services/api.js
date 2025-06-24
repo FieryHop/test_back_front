0954eb561d32c1b8dce6950bd73c4b3f72dd50d3
FieryHop <131314027+FieryHop@users.noreply.github.com>
2025-06-24 17:57:20 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://task-manager-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   },
