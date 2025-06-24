b9c5fa32a450340344b45e9dc88999ef1ce7a980
FieryHop <131314027+FieryHop@users.noreply.github.com>
2025-06-24 18:26:11 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,15 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'), 
     }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false
-      }
-    }
   }
 })
